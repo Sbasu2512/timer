fbf99e670a69b190b93d3826eb679270d0f51c54
Sayantan Basu <sayantanbasu@live.in>
2021-06-01 19:52:40 -0400

    in adding ifs

--- timer.js
+++ timer.js
@@ -1,21 +1,23 @@
 //Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments.
 // console.log("\007");
+const sound = () => process.stdout.write('\x07');
+const beep = mSecs => {
+ let delay = Number(mSecs);
+ setTimeout(sound,delay*1000);
+};
 const args = process.argv;
-const alarm = (args) => {
-let newArgs = args.splice(2);
-for(let ele of newArgs){
+
+// const music = (args) => {
+// let newArgs = args.slice(2)
+// console.log(newArgs);
+// }
+
+
+for(let ele of args){
   beep(ele);
 }
-};
 
 
-const beep = mSecs => {
-  let seconds = mSecs*1000;
-  setTimeout(()=>{
-    //console.log(' \007');
-    process.stdout.write('\x07');
-    },seconds)
-};
 
  
 
