7097089ac9ee4459551109f52e9673033d0191a7
Sayantan Basu <sayantanbasu@live.in>
2021-06-01 19:21:23 -0400

    exp stage -3

--- timer.js
+++ timer.js
@@ -1,13 +1,21 @@
 //Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments.
 // console.log("\007");
+const args = process.argv;
+const alarm = (args) => {
+let newArgs = args.splice(2);
+for(let ele of newArgs){
+  beep(ele);
+}
+};
+
+
 const beep = mSecs => {
   let seconds = mSecs*1000;
   setTimeout(()=>{
-    console.log(' \007');
-    //process.stdout.write(' \007');
+    //console.log(' \007');
+    process.stdout.write('\x07');
     },seconds)
-}
+};
 
-beep(10);
  
 
